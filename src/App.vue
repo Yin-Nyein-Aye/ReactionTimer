69ccefb8454318f137e3e9aebb13e7ce85e9e990
Yin-Nyein-Aye <yinnyeinaye.yinnyein@gmail.com>
2024-05-09 00:38:45 +0630

    Clickable game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>How fast you are?</h1>
+  <button @click="playGame" :disabled="isPlaying">play</button>
+  <div v-if="isPlaying">
+    <Block :delay="delay" @endGame="endGame" />
+  </div>
+  <div v-if="end">
+    <Result :score="score" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    Result
+  },
+  data(){
+    return{
+      isPlaying:false,
+      delay:null,
+      score:null,
+      end:false
+    }
+  },
+  methods:{
+    playGame(){
+      this.isPlaying = true;
+      this.delay=2000+Math.random()*5000;
+    },
+    endGame(score){
+      this.score=score
+      this.isPlaying=false
+      this.end = true
+    }
   }
 }
 </script>
---
+++ src/components/Block.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click Me</div>
+</template>
+
+<script>
+export default {
+    props:['delay'],
+    data(){
+        return{
+            showBlock:false,
+            score:0,
+            timer:null
+        }
+    },
+   mounted()
+   {
+        setTimeout(() => {
+            this.showBlock=true,
+            this.startTimer()
+        },this.delay);
+    },
+    methods:{
+        startTimer(){
+            this.timer=setInterval(() => {
+                this.score+=50; 
+                console.log(this.timer);               
+            }, 50);
+        },
+        stopTimer(){
+            clearInterval(this.timer);
+            this.$emit("endGame",this.score)
+        }
+    }
+    
+}
+</script>
+
+<style>
+.block{
+    background: rgb(141, 40, 40);
+    margin: 20px auto;
+    width: 300px;
+    height: 200px;
+    padding-top: 30px;
+    color:white;
+}
+</style>
